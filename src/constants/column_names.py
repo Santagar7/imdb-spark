7da7eeb5766c021210b40dca6531ae80c8a19517
vkaluzhnyi <vkaluzhnyi@vector-software.com>
2023-12-16 14:55:11 +0200

    add constants

---
+++ src/constants/column_names.py
@@ -0,0 +1,84 @@
+# Duplicating columns
+
+ordering = "ordering"
+nconst = "nconst"
+
+
+# Name basics dataframe columns
+
+primary_name = "primary_name"
+birth_year = "birth_year"
+death_year = "death_year"
+primary_profession = "primary_profession"
+known_for_titles = "known_for_titles"
+known_title = "known_title"
+title_count = "title_count"
+director_name = "director_name"
+writer_name = "writer_name"
+profession = "profession"
+
+
+# Titles akas dataframe columns
+
+title_id = "title_id"
+title = "title"
+region = "region"
+language = "language"
+types = "types"
+attributes = "attributes"
+is_original_title = "is_original_title"
+language_count = "language_count"
+titles_count = "titles_count"
+
+
+# Titles id column
+
+tconst = "tconst"
+
+
+# Titles basics dataframe columns
+
+title_type = "title_type"
+primary_title = "primary_title"
+original_title = "original_title"
+is_adult = "is_adult"
+start_year = "start_year"
+end_year = "end_year"
+runtime_minutes = "runtime_minutes"
+genres = "genres"
+genre = "genre"
+titles_avg_rating = "titles_avg_rating"
+genre_avg_rating = "genre_avg_rating"
+year_avg_rating = "year_avg_rating"
+year_count = "year_count"
+years_running = "years_running"
+
+
+# Titles crew dataframe columns
+
+directors = "directors"
+writers = "writers"
+director = "director"
+writer = "writer"
+genre_max_rating = "genre_max_rating"
+
+
+# Titles episode dataframe columns
+
+parent_tconst = "parent_tconst"
+season_number = "season_number"
+episode_number = "episode_number"
+max_episodes = "max_episodes"
+
+
+# Titles principals dataframe columns
+
+category = "category"
+job = "job"
+characters = "characters"
+
+
+# Titles ratings dataframe columns
+
+average_rating = "average_rating"
+num_votes = "num_votes"
