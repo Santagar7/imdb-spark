ef9ac4c56eedd15eb5533489c8233ea5b17ece84
vkaluzhnyi <vkaluzhnyi@vector-software.com>
2023-12-16 19:58:29 +0200

    Valentyn Kaluzhnyi's questions

--- src/Question.py
+++ src/Question.py
@@ -8,6 +8,8 @@ from src.schemas.title_basics_schema import title_basics_schema
 from src.schemas.title_crew_schema import title_crew_schema
 from src.schemas.title_principals_schema import title_principals_schema
 from src.schemas.title_ratings_schema import title_ratings_schema
+from src.schemas.title_akas_schema import title_akas_schema
+from src.schemas.title_episode_schema import title_episode_schema
 from src.constants.datasets_directories import DatasetsDirectories
 from src.constants.column_names import ColumnNames
 
@@ -22,8 +24,11 @@ class Question:
 
         self.title_ratings = TsvData(path=DatasetsDirectories.ratings, spark_session=self.spark_session,
                                      schema=title_ratings_schema).data
+        self.title_akas = TsvData(path=DatasetsDirectories.akas, spark_session=self.spark_session,
+                                  schema=title_akas_schema).data
         self.title_basics = TsvData(path=DatasetsDirectories.basics, spark_session=self.spark_session,
                                     schema=title_basics_schema).data
+        self.title_episodes = TsvData(path=DatasetsDirectories.episodes, spark_session=self.spark_session, schema=title_episode_schema).data
         self.title_basics = self.title_basics.withColumn(ColumnNames.genres,
                                                          f.split(f.col(ColumnNames.genres), ",").cast("array<string>"))
         self.title_crew = TsvData(path=DatasetsDirectories.crew, spark_session=self.spark_session,
@@ -128,3 +133,86 @@ class Question:
         rating_deviation.select(ColumnNames.tconst, ColumnNames.primaryTitle, ColumnNames.startYear, ColumnNames.genres,
                                 ColumnNames.averageRating, ColumnNames.avg_genre_rating,
                                 ColumnNames.rating_deviation).show()
+
+    def series_with_most_episodes(self):
+        window_spec = Window.partitionBy(ColumnNames.parentTconst)
+        episode_count = self.title_episodes.withColumn("episodeCount",
+                                                       f.count(ColumnNames.episodeNumber).over(window_spec))
+        top_series = episode_count.groupBy(ColumnNames.parentTconst).agg(f.max("episodeCount").alias("totalEpisodes"))
+        top_series_with_names = top_series.join(self.title_basics,
+                                                top_series[ColumnNames.parentTconst] == self.title_basics[
+                                                    ColumnNames.tconst]).select(ColumnNames.primaryTitle,
+                                                                                "totalEpisodes").orderBy(
+            f.desc("totalEpisodes")).limit(10)
+        top_series_with_names.show()
+
+    def shortest_movies(self):
+        # Вибираємо тільки ті записи, де тривалість вказана
+        non_null_movies = self.title_basics.filter(self.title_basics[ColumnNames.runtimeMinutes].isNotNull())
+
+        # Використовуємо Window function для сортування фільмів за тривалістю
+        window_spec = Window.partitionBy(ColumnNames.titleType).orderBy(ColumnNames.runtimeMinutes)
+        shortest_movies_ranked = non_null_movies.withColumn("rank", f.row_number().over(window_spec))
+
+        # Відбираємо топ 10 найкоротших фільмів
+        shortest_movies = shortest_movies_ranked.filter(shortest_movies_ranked.rank <= 10).select(
+            ColumnNames.primaryTitle, ColumnNames.runtimeMinutes)
+
+        shortest_movies.show()
+
+    def actors_with_biggest_rating_difference(self):
+        window_spec = Window.partitionBy(self.title_principals[ColumnNames.nconst])
+        actors_movies_with_ratings = self.title_principals.join(self.title_ratings,
+                                                                self.title_principals[ColumnNames.tconst] ==
+                                                                self.title_ratings[ColumnNames.tconst])
+        max_min_ratings = actors_movies_with_ratings.withColumn("maxRating", f.max(ColumnNames.averageRating).over(
+            window_spec)).withColumn("minRating", f.min(ColumnNames.averageRating).over(window_spec))
+        actors_rating_diff = max_min_ratings.withColumn("ratingDifference", f.col("maxRating") - f.col("minRating"))
+        top_actors_by_rating_diff = actors_rating_diff.join(self.name_basics,
+                                                            actors_rating_diff[ColumnNames.nconst] == self.name_basics[
+                                                                ColumnNames.nconst]).select(ColumnNames.primaryName,
+                                                                                            "ratingDifference").distinct().orderBy(
+            f.desc("ratingDifference")).limit(10)
+        top_actors_by_rating_diff.show()
+
+    def movies_with_most_adaptations(self):
+        window_spec = Window.partitionBy(ColumnNames.titleId)
+        adaptations_count = self.title_akas.withColumn("adaptationsCount", f.count(ColumnNames.title).over(window_spec))
+        top_movies_by_adaptations = adaptations_count.groupBy(ColumnNames.titleId).agg(
+            f.max("adaptationsCount").alias("totalAdaptations"))
+        top_movies_with_names = top_movies_by_adaptations.join(self.title_basics,
+                                                               top_movies_by_adaptations[ColumnNames.titleId] ==
+                                                               self.title_basics[ColumnNames.tconst]).select(
+            ColumnNames.primaryTitle, "totalAdaptations").orderBy(f.desc("totalAdaptations")).limit(10)
+        top_movies_with_names.show()
+
+    def actors_with_longest_careers(self):
+        actors_career_length = self.name_basics.withColumn("careerLength",
+                                                           f.col(ColumnNames.deathYear) - f.col(ColumnNames.birthYear))
+        window_spec = Window.orderBy(f.desc("careerLength"))
+        top_actors_by_career_length = actors_career_length.withColumn("rank", f.row_number().over(window_spec)).filter(
+            f.col("rank") <= 10)
+        top_actors_by_career_length.select(ColumnNames.primaryName, "careerLength").show()
+
+    def actors_in_most_animated_movies(self):
+        animated_movies = self.title_basics.filter(f.array_contains(f.col(ColumnNames.genres), "Animation"))
+        actors_in_animated = animated_movies.join(self.title_principals,
+                                                  animated_movies[ColumnNames.tconst] == self.title_principals[
+                                                      ColumnNames.tconst])
+
+        # Вказуємо конкретну таблицю для tconst
+        window_spec = Window.partitionBy(self.title_principals[ColumnNames.nconst])
+        actors_count = actors_in_animated.withColumn("animatedCount",
+                                                     f.count(self.title_principals[ColumnNames.tconst]).over(
+                                                         window_spec))
+
+        # Явно вказуємо таблицю для nconst
+        top_actors_in_animated = actors_count.join(self.name_basics,
+                                                   actors_count[ColumnNames.nconst] == self.name_basics[
+                                                       ColumnNames.nconst]).groupBy(
+            self.name_basics[ColumnNames.nconst], self.name_basics[ColumnNames.primaryName]).agg(
+            f.max("animatedCount").alias("totalAnimated")).orderBy(f.desc("totalAnimated")).limit(10)
+        top_actors_in_animated.show()
+
+
+
